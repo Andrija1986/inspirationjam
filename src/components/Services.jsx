66e4da66c459f20c5483f7f96aaec944558c14f0
andrija1986 <andrija1986@yahoo.com>
2022-06-11 12:57:59 +0200

    added text

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,21 +8,21 @@ const Services = () => {
       <div>
         <ImBriefcase/>
         <h3>Digital Strategy</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus enim odit assumenda, quaerat, blanditiis nisi error illum possimus culpa facere repellendus officia, soluta vel veritatis minus accusantium quod eum consectetur.
+        <p>Digital Strategy: Every Outstanding work is at first impossible. With the perfect Strategy and Planning, we weave every thread cautiously to make your business stand out in the Market.
         </p>
         <button>More</button>
       </div>
       <div>
           <BiPaintRoll/>
         <h3>Creative Marketing</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus enim odit assumenda, quaerat, blanditiis nisi error illum possimus culpa facere repellendus officia, soluta vel veritatis minus accusantium quod eum consectetur.
+        <p>Creative Marketing: Innovation distinguishes between the best. Our Creative Expert with profound imagination which is the true sign of intelligence brings you a variety of options to reality.
         </p>
         <button>More</button>
       </div>
       <div>
           <FcMultipleDevices/>
         <h3>Web Development</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus enim odit assumenda, quaerat, blanditiis nisi error illum possimus culpa facere repellendus officia, soluta vel veritatis minus accusantium quod eum consectetur.
+        <p>Web Development: Development paves the way for opportunities. Through pioneering techniques and Ideas, our development Teamm makes sure that they deliver the premier weaved sustainable designs.
         </p>
         <button>More</button>
       </div>

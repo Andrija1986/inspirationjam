c55d4657e8059e8ebd26ad7e6d56c25ddf98899d
Adanurk <gumussoykbr@gmail.com>
2022-06-12 12:32:19 +0200

    tech added

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -6,7 +6,72 @@ import { IconContext } from "react-icons";
 
 const Technology = () => {
   return (
-    <section id='technology' className='technology'>
+    <div id='technology' className="biggest">
+        <div className="main tech">
+            <div className="details">
+                <h1>Technology Stack</h1>
+            </div>
+            <div className="horizontal-scroll">
+                <div className="scroll-element">
+                    <h3>Backend</h3>
+                    <ul>
+                        <li>.NET</li>
+                        <li>Node</li>
+                        <li>Ruby</li>
+                        <li>Java</li>
+                        <li>PHP</li>
+                    </ul>
+                </div>
+                <div className="scroll-element">
+                    <h3>Frontend</h3>
+                    <ul>
+                        <li>React</li>
+                        <li>Vue</li>
+                        <li>Angular</li>
+                    </ul>
+                </div>
+                <div className="scroll-element">
+                    <h3>Database</h3>
+                    <ul>
+                        <li>SQL Server</li>
+                        <li>Postgre SQL</li>
+                        <li>MySQL</li>
+                        <li>MongoDB</li>
+                        <li>Oracle</li>
+                    </ul>
+                </div>
+                <div className="scroll-element">
+                    <h3>Mobility</h3>
+                    <ul>
+                        <li>iOS</li>
+                        <li>React Native</li>
+                        <li>Android</li>
+                    </ul>
+                </div>
+                <div className="scroll-element">
+                    <h3>title</h3>
+                    <ul>
+                        <li>list</li>
+                        <li>list</li>
+                    </ul>
+                </div>
+                <div className="scroll-element">
+                    <h3>title</h3>
+                    <ul>
+                        <li>list</li>
+                        <li>list</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Technology
+
+/* <section id='technology' className='technology'>
     <IconContext.Provider value={{className: "top-icons"}}>
       <SiJavascript/>
       <IoLogoAngular/>
@@ -17,8 +82,4 @@ const Technology = () => {
       <IoLogoNodejs/>
       <SiPhp/>
     </IconContext.Provider>
-    </section>
-  )
-}
-
-export default Technology
+    </section> */

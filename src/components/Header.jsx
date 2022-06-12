6675659c2a1127342343933acb2af68708390362
andrija1986 <andrija1986@yahoo.com>
2022-06-12 22:24:08 +0200

    correction on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 
       <p><span className="words">Vision:</span> Give businesses innovative strategic design  and drive a new era of Marketing. </p>
 
-      <p><span className="words">Mission:</span> Our team works with businesses to provide creative designs and to deliver market-defining high-quality solutions that create value and consistent competitive advantage for our clients. Besides providing innovative services and using proven strategic methods, we give your customers that “WOW, This is what I was looking for” feeling when they come across the designs and marketing channels, we have helped you with.</p>
+      <p><span className="words">Mission:</span> Our team works with businesses to provide creative designs and to deliver market-defining high-quality solutions that create value and consistent competitive advantage for our clients.</p>
   </div>
       
     </header>

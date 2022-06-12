bbb1e068fd86ac1d49832bc84ecb43532889ec46
Adanurk <gumussoykbr@gmail.com>
2022-06-12 12:15:32 +0200

    industries added

--- src/components/Industries.jsx
+++ src/components/Industries.jsx
@@ -2,15 +2,51 @@ import React from 'react'
 
 const Industries = () => {
   return (
-    <section id='industries' className='industries'>
+    <div className="biggest" id='industries'>
+        <div className="main cards-wrapper">
+            <div className="cards-heading">
+                <p>We outshine at delivering the best web development and marketing services as per the customers’
+                    requirements, be it small-size companies in their beginning phase or mid-size businesses looking for
+                    growth, or large organizations actively making efforts to expand.</p>
+            </div>
+            <div className="cards-container-industry">
+                <div className="card-industry">
+                    <img src="#" alt="some-here"/>
+                    <h4>Education</h4>
+                </div>
+                <div className="card-industry">
+                    <img src="#" alt="some-here"/>
+                    <h4>Healthcare</h4>
+                </div>
+                <div className="card-industry">
+                    <img src="#" alt="some-here"/>
+                    <h4>Retail and Ecommerce</h4>
+                </div>
+                <div className="card-industry">
+                    <img src="#" alt="some-here"/>
+                    <h4>Travel</h4>
+                </div>
+                <div className="card-industry">
+                    <img src="#" alt="some-here"/>
+                    <h4>Public Sector</h4>
+                </div>
+                <div className="card-industry">
+                    <img src="#" alt="some-here"/>
+                    <h4>Logistics</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Industries
+
+/* <section id='industries' className='industries'>
       <div>Education</div>
       <div>Health Care</div>
       <div>Media and Entertainment</div>
       <div>Retail and Commerce</div>
       <div>Travel and Hospitality</div>
       <div>Logistics and Distribution</div>
-    </section>
-  )
-}
-
-export default Industries
+    </section> */

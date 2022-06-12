1528f22f34c47269ffe18ff3ca8211552e74649b
andrija1986 <andrija1986@yahoo.com>
2022-06-12 12:15:51 +0200

    pictures

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,17 +6,17 @@ const Testimonials = () => {
   return (
     <section id='testimonials' className='testimonials'>
       <div className="start">
-        <img src="./assets/wavers1.png" alt="customer1" />
+        <img src="./assets/sarah.jpg" alt="customer1" />
         <p>"Letting things grind and flow, for the maximum result. Putting the customer first is the Wever's DNA. The team keeps you sharp! Great, in the dynamics of everyday life."</p>
         <p>Sarah Livne</p>
       </div>
       <div className="middle">
-        <img src="./assets/wevers2.png" alt="customer2" />
+        <img src="./assets/elija.jpg" alt="customer2" />
         <p>“Wevers is a remarkable Online Marketing Agency. The Weavers are top-notch professionals and among the hardest working, most creative, and innovative people - They know their stuff, inside and out, and have consistently delivered amazing results to us over the past 5 years. I highly recommend their services.”</p>
         <p>Elija's and Oliver's Soundlab</p>
       </div>
       <div className="end">
-        <img src="./assets/wevers3.png" alt="customer3" />
+        <img src="./assets/devyn.jpg" alt="customer3" />
         <p>Without a doubt one of the most talented programmers out there. I always go back to the Wevers when I'm out of my depth and he's never failed to deliver what I ask for. Smart, trustworthy, and professional. You won't be disappointed.</p>
         <p>Devyn</p>
       </div>

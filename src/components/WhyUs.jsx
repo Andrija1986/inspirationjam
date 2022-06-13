2451d09542089a6013a2aa1da0c189e0204cabf0
andrija1986 <andrija1986@yahoo.com>
2022-06-13 09:52:06 +0200

    responsive

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -12,21 +12,21 @@ const WhyUs = () => {
                         <img src="./assets/number1.png" alt="num1" className="nums"/>
                         <h3>Freshness</h3>
                         </div>
-                        <p>advanced thinking developers and Marketers with higher business acumen to meet your project demands.</p>
+                        <p>Advanced thinking developers and Marketers with higher business acumen to meet your project demands.</p>
                     </div>
                     <div className="reasons-sent">
                         <div className="experiment">
                         <img src="./assets/number2.png" alt="num2" className="nums"/>
                         <h3>One-Stop solution</h3>
                         </div>
-                        <p>for all your unique requirements, our dedicated teams ensure remarkable results.</p>
+                        <p>For all your unique requirements, our dedicated teams ensure remarkable results.</p>
                     </div>
                     <div className="reasons-sent">
                     <div className="experiment">
                         <img src="./assets/number3.png" alt="num3" className="nums"/>
                         <h3>Timely Delivery</h3>
                         </div>
-                        <p>time-driven projects and services are considered best business practices for proven results, and innovation.</p>
+                        <p>Time-driven projects and services are considered best business practices for proven results, and innovation.</p>
                     </div>
                 </div>
                 <img src="./assets/why-us.jpg" alt="" className="img-2"/>
